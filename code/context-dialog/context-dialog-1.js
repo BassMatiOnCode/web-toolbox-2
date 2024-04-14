47b63ca47f8c42af7307bf92f06718861cb51436
Bassmati on Code <75748577+BassMatiOnCode@users.noreply.github.com>
2024-04-14 19:24:34 +0200

    context-dialog component: adjustements and debugging.

--- code/context-dialog/context-dialog-1.js
+++ code/context-dialog/context-dialog-1.js
@@ -7,63 +7,80 @@
 "use strict" ;
 	// The namespace for the package.
 if ( typeof Synesis === "undefined" ) var Synesis = { } ;
-if ( ! Synesis.Page ) Synesis.Page = { } ;
-	// Holds a list of context dialogs with associated information, event handlers, ...
-Synesis.Page.contextDialogRegistry = { } ;
-	// Points to the currently open dialog.
-Synesis.Page.activeContextDialogDescriptor = null ;
-	// Points to the element on which the context dialog is shown.
-Synesis.Page.activeContextElement = null ;
-	// Points to the active context parent element
-Synesis.Page.activeContextParent = null ;
+if ( ! Synesis.ContextDialog ) Synesis.ContextDialog = { } ;
 
-/** 
-*	Registers a context dialog and its event handlers.
+	// List of context dialog descriptors
+Synesis.ContextDialog.registry = { } ;
+	// Points to the currently open dialog:
+Synesis.ContextDialog.activeDescriptor = null ;
+	// Points to the element which received the right-click:
+Synesis.ContextDialog.activeElement = null ;
+	// Points to the active context parent element:
+Synesis.ContextDialog.activeParent = null ;
+
+/** register( )
+*	Registers a context dialog and its associated open and close dialog event handlers.
 *	@param {string} dialogid - ID of the context dialog element
-*	@param {function} [onopen] - called before the dialog is opened
-*	@param {function} [onclose] - called before the dialog is closed.
+*	@param {function} [dialogOpening] - called before the dialog is opened
+*	@param {function} [dialogClosing] - called before the dialog is closed.
 *
-*/ Synesis.Page.registerContextDialog = function ( dialogid, onopen=null, onclose=null ) {
+*/ Synesis.ContextDialog.register = function ( dialogid, dialogOpening=null, dialogClosing=null ) {
+	//	Prevent duplicate keys
+	if ( this.registry[ dialogid ] ) return console.error( "The dialog ID is already registered." );
 	//	Find the dialog element
 	const dialog = document.getElementById( dialogid );
+	if ( ! dialog ) return console.error( `Context dialog ${dialogid} not found.` );
 	//	Create a registry entry
-	this.contextDialogRegistry[ dialogid ] = {
+	const descriptor = this.registry[ dialogid ] = {
 		dialog : dialog ,		// element id of the dialog
-		onopen : onopen ,	// called when before the dialog is opened
-		onclose : onclose		// called when the dialog is closed
+		dialogOpening : dialogOpening ,	// called when before the dialog is opened
+		dialogClosing : dialogClosing		// called when the dialog is closed
 		} ;
-	if ( ! dialog.querySelector( "[data-close-dialog]" )) dialog.toggleAttribute( "data-dialog-autoclose", true );
-	// Register a click event handler to close the dialog
+	// Set the autoclose attribute on the dialog element if a click on any elemeny closes the dialog.
+	if ( ! dialog.querySelector( "[data-contextdialog-close]" )) dialog.toggleAttribute( "data-contextdialog-autoclose", true );
+	// Add a click event listener to the dialog element.
 	dialog.addEventListener( "click" , evt => {
 		// Find the dialog container element
-		const dialog = evt.target.closest( ".context-dialog" );
-		if ( ! dialog.hasAttribute( "data-dialog-autoclose" ) && ! evt.target.hasAttribute( "data-close-dialog" )) return;
+		const descriptor = Synesis.ContextDialog.activeDescriptor;
+		if ( ! descriptor.dialog.hasAttribute( "data-contextdialog-autoclose" ) && ! evt.target.hasAttribute( "data-contextdialog-close" )) return evt.stopPropagation();
 		//	Notify caller about dialog closing
-		if ( Synesis.Page.contextDialogRegistry[dialog.id].onclose( evt, dialog ) === false ) return ;
-		Synesis.Page.closeContextDialog( );
+		if ( descriptor.dialogClosing && descriptor.dialogClosing( evt, dialog ) === false ) return ;
+//		Synesis.ContextDialog.closeContextDialog( );
 		} ) ;
 	} ;
-/** Untility function */ Synesis.Page.closeContextDialog = function ( ) {
-	//	Close the active context dialog if open.
-	if ( ! Synesis.Page.activeContextDialogDescriptor ) return ;
-	Synesis.Page.activeContextDialogDescriptor.dialog.style.visibility = "hidden" ;
-	console.log( "Context dialog closed" );
-	Synesis.Page.activeContextDialogDescriptor = null ;
-	Synesis.Page.activeContextElement = null ;
-	Synesis.Page.activeContextParent = null ;
+/**	closeContextDialog( )
+ *	Hides the context dialog and clears the context state variables.
+ *	
+ */ Synesis.ContextDialog.closeContextDialog = function ( ) {
+	//	Bail out if there is no active context dialog.
+	if ( ! Synesis.ContextDialog.activeDescriptor ) return ;
+	//	Hide dialog.
+	Synesis.ContextDialog.activeDescriptor.dialog.style.visibility = "hidden" ;
+	//	Reset state variables.
+	Synesis.ContextDialog.activeDescriptor = null ;
+	Synesis.ContextDialog.activeElement = null ;
+	Synesis.ContextDialog.activeParent = null ;
 	} ;
-/** Script init code */ ( function ( ) {
-	//	Register event handler to open a context dialog
+/** Script init code 
+ *	Adds a contextment and click event listeners to the document to open and close a context dialog.
+ *	
+ */ ( function ( ) {
+	//	Register contextmenu event handler
 	document.addEventListener( "contextmenu", evt => {
 		//	Find the context dialog id
-		Synesis.Page.activeContextElement = evt.target ;
-		Synesis.Page.activeContextParent = evt.target.closest( "[data-contextdialog-id-parent]" )
-		let dialogid = evt.target.getAttribute( "data-contextdialog-id" );
-		if ( ! dialogid ) dialogid = Synesis.Page.activeContextParent?.getAttribute( "data-contextdialog-id-parent" );
-		if ( ! dialogid ) return Synesis.Page.closeContextDialog( );
+		Synesis.ContextDialog.activeElement = evt.target.closest( "[data-contextdialog-id]" );
+		const dialogid = Synesis.ContextDialog.activeElement?.getAttribute( "data-contextdialog-id" );
+		//	Cancel if there is none.
+		if ( ! dialogid ) return Synesis.ContextDialog.closeContextDialog( );
+		//	Adjust pointers if it is a context parent element
+		if ( Synesis.ContextDialog.activeElement.hasAttribute( "data-contextdialog-parent" )) {
+			Synesis.ContextDialog.activeParent = Synesis.ContextDialog.activeElement;
+			Synesis.ContextDialog.activeElement = evt.target;
+			}
 		//	Position the dialog.
 		evt.preventDefault( );
-		const descriptor = Synesis.Page.activeContextDialogDescriptor = Synesis.Page.contextDialogRegistry[ dialogid ];
+		const descriptor = Synesis.ContextDialog.activeDescriptor = Synesis.ContextDialog.registry[ dialogid ];
+		if ( ! descriptor ) return console.error( `Context dialog "${dialogid}" is not registered.` );
 		const dialog = descriptor.dialog;
 		let y = evt.target.getBoundingClientRect().top + window.visualViewport.pageTop;
 		let x = evt.target.getBoundingClientRect().left + window.visualViewport.pageLeft;
@@ -76,15 +93,15 @@ Synesis.Page.activeContextParent = null ;
 		*/
 		dialog.style.left = x + "px";
 		dialog.style.top = y + "px";
+		//	Cancel opening if dialogOpening function returns false.
+		if ( descriptor.dialogOpening && descriptor.dialogOpening( evt, dialog ) === false ) return Synesis.ContextDialog.closeContextDialog( );
 		//	Open the dialog.
-		if ( descriptor.onopen( evt, dialog ) === false ) return Synesis.Page.closeContextDialog( );
 		dialog.style.visibility = "visible" ;
 		} ) ;
-	//	Add a click event handler to the document 
-	//	to catch clicks outside the context dialog.
+	//	Add a click event handler to the document to catch clicks outside the context dialog.
 	document.addEventListener( "click", evt => {
 		if ( evt.target.tagName === "A" ) return ;
-		Synesis.Page.closeContextDialog( );
+		Synesis.ContextDialog.closeContextDialog( );
 		} ) ;
 	} ) ( ) ;
 
